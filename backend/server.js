283b9afedca615b1197caed64abbe24d0748b58b
manugunnala <manugunnala@gmail.com>
2020-09-02 21:54:37 -0500

    first commit - add local project to repo

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+
+//set up environment variables 
+const express = require ('express');
+const cors = require('cors'); 
+const mongoose = require('mongoose');
+
+require('dotenv').config(); 
+
+//set up server 
+const app = express(); 
+const port = process.env.PORT || 5000;
+
+app.use(cors()); 
+app.use(express.json()); 
+
+//connect to mongoDB
+const uri = process.env.ATLAS_URI; 
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection successfully established'); 
+})
+
+//require and use router files
+const exercisesRouter = require('./routes/exercises'); 
+const usersRouter = require('./routes/users'); 
+app.use('/exercises', exercisesRouter); 
+app.use('/users', usersRouter);
+
+//start server
+app.listen(port, () => { 
+    console.log('Server is running on port: ' + port); 
+});
